1150e11168e33f4eb792db147ff6eb940fd8257f
anatolii_kim <anatolii_kim@epam.com>
2023-07-12 00:22:01 +0500

    architect: ts, html and scss linters added

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,45 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: 'standard-with-typescript',
   overrides: [
+    {
+      "files": ["*.ts"],
+      "extends": [
+        "eslint:recommended",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      "rules": {
+        "@angular-eslint/directive-selector": [
+          "error",
+          { "type": "attribute", "prefix": "kb", "style": "camelCase" }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          { "type": "element", "prefix": "kb", "style": "kebab-case" }
+        ],
+        "max-len": [
+          "error",
+          {
+            "ignorePattern": "^import .*",
+            "ignoreUrls": true,
+            "code": 150
+          }
+        ]
+      }
+    },
+    {
+      "files": [
+        "*.html"
+      ],
+      "extends": [
+        "plugin:@angular-eslint/template/recommended"
+      ],
+      "rules": {}
+    }
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+  parserOptions: { 
+    "project": ["tsconfig.json"] 
   },
   rules: {
   }
