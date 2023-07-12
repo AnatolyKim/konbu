57e5e50170e88531c676a5673e8287220ab36598
anatolii_kim <anatolii_kim@epam.com>
2023-07-13 00:56:48 +0500

    architect: prettier package added with initial formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,39 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   overrides: [
     {
-      "files": ["*.ts"],
-      "extends": [
-        "eslint:recommended",
-        "plugin:@angular-eslint/recommended",
-        "plugin:@typescript-eslint/recommended",
-      ],
-      "rules": {
-        "@angular-eslint/directive-selector": [
-          "error",
-          { "type": "attribute", "prefix": "kb", "style": "camelCase" }
-        ],
-        "@angular-eslint/component-selector": [
-          "error",
-          { "type": "element", "prefix": "kb", "style": "kebab-case" }
-        ],
-        "max-len": [
-          "error",
+      files: ['*.ts'],
+      extends: ['eslint:recommended', 'plugin:@angular-eslint/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+      rules: {
+        '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'kb', style: 'camelCase' }],
+        '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'kb', style: 'kebab-case' }],
+        'max-len': [
+          'error',
           {
-            "ignorePattern": "^import .*",
-            "ignoreUrls": true,
-            "code": 150
+            ignorePattern: '^import .*',
+            ignoreUrls: true,
+            code: 150,
+          },
+        ],
+        "array-element-newline": [
+          'error', {
+          'ArrayExpression': 'consistent',
+          'ArrayPattern': { 'minItems': 3 },
           }
         ]
-      }
+      },
     },
     {
-      "files": [
-        "*.html"
-      ],
-      "extends": [
-        "plugin:@angular-eslint/template/recommended"
-      ],
-      "rules": {}
-    }
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {},
+    },
   ],
-  parserOptions: { 
-    "project": ["tsconfig.json"] 
+  parserOptions: {
+    project: ['tsconfig.json'],
   },
-  rules: {
-  }
-}
+  rules: {},
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'kb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'konbu-main';
