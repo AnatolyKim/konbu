879dce9a283e8a6d90a526f3e6921319f6f62bbc
anatolii_kim <anatolii_kim@epam.com>
2023-07-14 01:55:20 +0500

    architect: husky package with commit linter added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('konbu-main');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('konbu-main app is running!');
-  });
 });
